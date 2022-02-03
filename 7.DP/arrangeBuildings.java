013e1cadd17d200baff37eb4b46c6a6c043ccf9d
mktintumon <mktintumon@gmail.com>
2022-02-03 15:22:31 +0530

    added

--- 7.DP/arrangeBuildings.java
+++ 7.DP/arrangeBuildings.java
@@ -1,3 +1,33 @@
-public class arrangeBuildings {
-    
+import java.util.*;
+
+public class arrangeBuildings{
+    //same as count binary string 
+
+    public static int arrangeBuild(int n){
+
+        int oldBuilding = 1;
+        int oldSpace = 1;
+
+        for(int i=2 ; i<=n ; i++){
+             int nBuilding = oldSpace;
+             int nSpace = oldSpace + oldBuilding ;
+
+             oldBuilding = nBuilding;
+             oldSpace =  nSpace;
+
+        }
+
+        int total = oldBuilding + oldSpace;
+        return total; 
+    }
+
+public static void main(String[] args) throws Exception {
+      Scanner sc = new Scanner(System.in);
+
+      int n = sc.nextInt();
+      long ans = arrangeBuild(n); // ans cant store in int , so change to long
+      System.out.println(ans*ans);
+      sc.close();
+ }
+
 }
