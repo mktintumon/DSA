f5838a31ee0c7673902321b5df2e8eb139f2e96c
mktintumon <mktintumon@gmail.com>
2022-03-10 02:19:47 +0530

    added

--- Extras/phoneBooth.java
+++ Extras/phoneBooth.java
@@ -1,6 +1,55 @@
 import java.util.Scanner;
 
 public class phoneBooth {
+
+    public static boolean booth(int[] arr , int k){
+        int n = arr.length;
+        
+        if(k==0){
+            return true;
+        }
+        
+
+        if(n == 1 && arr[0] == 0){
+            if(k==0){
+                return false;
+            }
+            else if(k==1){
+                return true;
+            }
+        }
+        
+
+        int count = 0;
+        for(int i=0 ; i<n ; i++){
+           if(i == 0){
+               if(arr[i] == 0 && arr[i+1] == 0){
+                   count++;
+                   i++;
+               }
+           }
+            else if(i == n-1){
+                if(arr[i] == 0 && arr[i-1] == 0){
+                   count++;
+                    i++;
+               }
+            }
+            else{
+                if(arr[i] == 0 && arr[i-1] == 0 && arr[i+1] == 0){
+                   count++;
+                    i++;
+               }
+            }
+        }
+        
+        if(count >= k){
+           return true;
+        }
+        else{
+             return false;
+        }
+    }
+    
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
@@ -11,32 +60,8 @@ public class phoneBooth {
 
         int k = scn.nextInt();
 
-        int count = 0;
-        for(int i=0 ; i<n ; i++){
-           if(i == 0){
-               if(arr[i] == 0 && arr[i+1] == 0){
-                   count++;
-               }
-           }
-            else if(i == n-1){
-                if(arr[i] == 0 && arr[i-1] == 0){
-                   count++;
-               }
-            }
-            else{
-                if(arr[i] == 0 && arr[i-1] == 0 && arr[i+1] == 0){
-                   count++;
-               }
-            }
-        }
-        
-        if(count >= k){
-           System.out.println("true");
-        }
-        else{
-             System.out.println("false");
-        }
-
+        boolean ans = booth(arr , k);
+        System.out.println(ans);
         scn.close();
     }
 }
