d7ab6b85bce19f7785b55fe5e4a85ea2f13e1cca
mktintumon <mktintumon@gmail.com>
2022-03-11 00:56:45 +0530

    added

--- 07.DP/floorTilingMx1.java
+++ 07.DP/floorTilingMx1.java
@@ -1,36 +1,27 @@
 import java.util.*;
 
-public class floorTilingMx1{
-
-    public static int tiling(int n , int m){
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-
-        //Three cases to handle
-        for(int i=3 ; i<=n ; i++){
-
+public class floorTilingMx1 {
+    public static int ways(int n, int m){
+        int [] dp = new int[n+1];
+        for(int i = 1; i<=n; i++){
             if(i < m){
-                dp[i]  = 1; // only horizontal placement
+                dp[i] = 1;
             }
-            else if(i == m){ // only hori and vert placement , no other possible
-                dp[i] = 2 ; 
+            else if(i == m){
+                dp[i] = 2;
             }
-            else{ // all others except above two
-
+            else{
                 dp[i] = dp[i-1] + dp[i-m];
             }
         }
-
         return dp[n];
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        System.out.println(tiling(n,m));
-        sc.close();
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int ans = ways(n,m);
+        System.out.println(ans);
+        scn.close();
     }
 }
