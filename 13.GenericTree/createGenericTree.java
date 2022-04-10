2f746704a8d67b9e3d7535e152d5a075d28aca47
mktintumon <mktintumon@gmail.com>
2022-04-10 21:25:39 +0530

    added

---
+++ 13.GenericTree/createGenericTree.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.util.*;
+
+/*
+
+   1. create Generic Tree
+   2. Find size
+   3. Find Max
+   4. Find height 
+
+*/
+
+public class createGenericTree {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  public static int size(Node node){
+    if(node == null) return 0;
+
+    int s=0;
+    for(Node child : node.children){
+      s += size(child);
+    }
+
+    return s+1;
+  }
+
+
+  public static int max(Node node) {
+    int maxAns = Integer.MIN_VALUE;
+
+    for(Node child : node.children){
+        int maxChild = max(child);
+        maxAns = Math.max(maxChild,maxAns);
+    }
+
+    return Math.max(maxAns , node.data);
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    Node root = construct(arr);
+    int sz = size(root);
+    System.out.println(sz);
+    // display(root);
+  }
+
+}
